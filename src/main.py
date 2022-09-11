c79bbb6ba9f408d758925d7053dd2f628d4bf768
Issa Memari <issa.memari@gmail.com>
2022-09-11 11:53:00 +0200

    Better logging

--- src/main.py
+++ src/main.py
@@ -27,15 +27,13 @@ def worker(booker, booked_event, search_url, court_ids, date, time):
             dic = {
                 "thread_id": get_ident(),
                 "exception": str(e),
-                "timestamp": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
             }
-            logging.info("expected exception", dic)
+            logging.error("expected exception", dic)
             continue
         except Exception as e:
             dic = {
                 "thread_id": get_ident(),
                 "exception": str(e),
-                "timestamp": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
                 "stacktrace": traceback.format_exc(),
             }
             logging.error("unexpected exception", dic)
@@ -44,7 +42,6 @@ def worker(booker, booked_event, search_url, court_ids, date, time):
             booked_event.set()
             dic = {
                 "thread_id": get_ident(),
-                "timestamp": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
             }
             logging.info("court booked", dic)
             break
@@ -58,9 +55,10 @@ def main():
     parser.add_argument("--time", type=str, help="format: 08h", required=True)
     parser.add_argument("--workers", type=int, default=4)
     parser.add_argument("--headless", action="store_true", default=False)
+    parser.add_argument("--logger-pretty", action="store_true", default=False)
     args = parser.parse_args()
 
-    zlog.configure(pretty=True)
+    zlog.configure(pretty=args.logger_pretty)
 
     with open(args.config, "r") as f:
         config = json.load(f)
